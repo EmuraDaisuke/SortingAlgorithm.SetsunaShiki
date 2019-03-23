d97ea8193f18a1e7888b803272a17bb7495e8e01
dia_maisun <dia_maisun@jcom.home.ne.jp>
2019-03-23 13:37:56 +0900

    .

---
+++ Main.cpp
@@ -0,0 +1,151 @@
+
+
+
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+#include "./SetsunaShiki.h"
+#include "./Lapse.h"
+
+
+
+enum class eSrc {
+    Min,
+    Max,
+    Flat,
+};
+
+using sort_t = float;
+
+
+
+#define ORDER   0
+#define FAT     0
+
+struct Test
+{
+    sort_t m;
+    #if ORDER//[
+    std::size_t o;
+    #endif//]
+    #if FAT//[
+    std::size_t f[FAT];
+    #endif
+};
+
+bool operator <(const Test& s, const Test& t)
+{
+    return s.m < t.m;
+}
+
+bool operator ==(const Test& s, const Test& t)
+{
+    return s.m == t.m;
+}
+
+
+
+void test(eSrc Src, int nTest, int nLoop, int nRepeat)
+{
+    auto a = std::vector<Test>(nTest);
+    for (auto n = nLoop; n; --n){
+        printf("\n\n--- %d\n", nTest);
+        std::size_t oChange = 0;
+        
+        switch (Src){
+            case eSrc::Min:{
+                sort_t m = 0;
+                for (auto& v : a) v.m = m++;
+                
+                oChange = a.size()-1;
+                a[oChange].m = -1;
+                break;
+            }
+            case eSrc::Max:{
+                sort_t m = 0;
+                for (auto& v : a) v.m = m++;
+                
+                oChange = 0;
+                a[oChange].m = a.size();
+                break;
+            }
+            case eSrc::Flat:{
+                sort_t m = 0;
+                for (auto& v : a) v.m = m;
+                break;
+            }
+        }
+        
+        {   // 
+            std::size_t o = 0;
+            for (auto& v : a){
+                #if ORDER//[
+                v.o = ++o;
+                #endif//]
+                #if FAT//[
+                for (auto& f : v.f) f = 0;
+                #endif//]
+            }
+        }
+        
+        #if 1//[
+        printf("\n== std::sort\n");
+        for (auto n = nRepeat; n; --n){
+            auto s = a;
+            Lapse l;
+            std::sort(s.begin(), s.end());
+        }
+        #endif//]
+        
+        #if 1//[
+        printf("\n== std::stable_sort\n");
+        for (auto n = nRepeat; n; --n){
+            auto s = a;
+            Lapse l;
+            std::stable_sort(s.begin(), s.end());
+        }
+        #endif//]
+        
+        #if 1//[
+        printf("\n== SetsunaShiki::Sort\n");
+        for (auto n = nRepeat; n; --n){
+            auto s = a;
+            Lapse l;
+            SetsunaShiki::Sort(s.data(), s.size(), oChange);
+        }
+        #endif//]
+        
+        #if 0//[
+        {   // 
+            auto s0 = a;
+            auto s1 = a;
+            auto s2 = a;
+            
+            std::sort(s0.begin(), s0.end());
+            std::stable_sort(s1.begin(), s1.end());
+            SetsunaShiki::Sort(s2.data(), s2.size(), oChange);
+            
+            auto bStrict01 = (memcmp(s0.data(), s1.data(), s1.size() * sizeof(Test)) == 0);
+            auto bStrict12 = (memcmp(s1.data(), s2.data(), s2.size() * sizeof(Test)) == 0);
+            auto bStrict20 = (memcmp(s2.data(), s0.data(), s0.size() * sizeof(Test)) == 0);
+            
+            printf("\n");
+            printf("%d %d %d\n", (s1 == s0), bStrict01, (a == s0));
+            printf("%d %d %d\n", (s2 == s1), bStrict12, (a == s1));
+            printf("%d %d %d\n", (s0 == s2), bStrict20, (a == s2));
+        }
+        #endif//]
+    }
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    eSrc Src = eSrc::Min;
+    test(Src,     10000, 1, 4);
+    test(Src,   1000000, 1, 4);
+    test(Src, 100000000, 1, 4);
+    return 0;
+}
---
+++ SetsunaShiki.h
@@ -0,0 +1,106 @@
+#pragma once
+// Copyright(c) 2018 Emura Daisuke
+// Apache License 2.0
+
+
+
+#include <cstddef>
+#include <utility>
+
+#define Auto    decltype(auto)
+
+
+
+namespace SetsunaShiki {
+
+
+
+// 
+
+template <class T> void Sort(T* const aSrc, std::size_t nSrc, std::size_t oChange);
+
+template <class T> std::size_t SearchLower(T* a, std::size_t n, T& v);
+template <class T> std::size_t SearchUpper(T* a, std::size_t n, T& v);
+
+template <class T> void CopyLower(T* p, std::size_t n);
+template <class T> void CopyUpper(T* p, std::size_t n);
+
+
+
+// 
+
+template <class T>
+void Sort(T* const aSrc, std::size_t nSrc, std::size_t oChange)
+{
+    if (aSrc && nSrc > 1 && oChange < nSrc){
+        auto nLower = oChange;
+        auto nUpper = nSrc - oChange - 1;
+        if (nLower && aSrc[oChange] < aSrc[oChange-1]){
+            Auto oLower = SearchLower(&aSrc[0], nLower, aSrc[oChange]);
+            CopyLower(&aSrc[oChange], (oChange - oLower));
+        } else if (nUpper && aSrc[oChange+1] < aSrc[oChange]){
+            Auto oUpper = SearchUpper(&aSrc[oChange+1], nUpper, aSrc[oChange]);
+            CopyUpper(&aSrc[oChange], (oUpper - oChange));
+        }
+    }
+}
+
+
+
+template <class T>
+std::size_t SearchLower(T* a, std::size_t n, T& v)
+{
+    std::size_t o0 = 0;
+    std::size_t o1 = n;
+    while ((o1 - o0) > 1){
+        auto oh = (o1 + o0) >> 1;
+        if (v < a[oh]){
+            o1 = oh;
+        } else {
+            o0 = oh;
+        }
+    }
+    return (v < a[o0])? o0: o1;
+}
+
+
+
+template <class T>
+std::size_t SearchUpper(T* a, std::size_t n, T& v)
+{
+    std::size_t o0 = 0;
+    std::size_t o1 = n;
+    while ((o1 - o0) > 1){
+        auto oh = (o1 + o0) >> 1;
+        if (a[oh] < v){
+            o0 = oh;
+        } else {
+            o1 = oh;
+        }
+    }
+    return o1;
+}
+
+
+
+template <class T>
+void CopyLower(T* p, std::size_t n)
+{
+    auto v = std::move(p[0]);
+    for (; n--; --p) p[0] = std::move(p[-1]);
+    p[0] = std::move(v);
+}
+
+
+
+template <class T>
+void CopyUpper(T* p, std::size_t n)
+{
+    auto v = std::move(p[0]);
+    for (; n--; ++p) p[0] = std::move(p[1]);
+    p[0] = std::move(v);
+}
+
+
+
+}
